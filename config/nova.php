2ea66c3ed5bf6e7e4ebb54dacf4a12aec12c01fa
Rhys Lees <43909932+RhysLees@users.noreply.github.com>
2022-04-14 23:56:36 +0100

    Impersonation Fix

--- config/nova.php
+++ config/nova.php
@@ -100,6 +100,7 @@ return [
 
     'middleware' => [
         'web',
+        'auth.session',
         HandleInertiaRequests::class,
         DispatchServingNovaEvent::class,
         BootTools::class,
